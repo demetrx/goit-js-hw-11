341cae16cebdc673101a6370cf2ef9589f5b5fc0
demetrx <mcbright7447@gmail.com>
2022-05-03 21:50:20 +0300

    complete

---
+++ src/index.js
@@ -0,0 +1,120 @@
+import './sass/main.scss';
+import throttle from 'lodash.throttle';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import QueryAPI from './js/query-api.js';
+import photoCardsTpl from './templates/photo-cards.hbs';
+
+const queryAPI = new QueryAPI();
+const lightbox = new SimpleLightbox('.gallery a');
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  gallery: document.querySelector('#gallery'),
+  loadMoreBtn: document.querySelector('#load-more'),
+};
+
+let totalHits = 0;
+let isEndReached = false;
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+window.addEventListener('scroll', throttle(makeInfScroll, 500));
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const value = e.currentTarget.elements.searchQuery.value;
+
+  queryAPI.setSearchQuery(value);
+  queryAPI.fetchImgs().then(onSearchFinished).catch(onError);
+}
+
+function onLoadMore() {
+  if (isEndReached) {
+    return;
+  }
+
+  disableLoadMoreBtn();
+
+  if (totalHits <= queryAPI.getFetchedElsNum()) {
+    isEndReached = true;
+    notifyOfEndOfResults();
+    return;
+  }
+
+  queryAPI.fetchImgs().then(onLoadMoreFinished).catch(onError);
+}
+
+function onSearchFinished(data) {
+  clearGallery();
+
+  const matches = data.hits;
+  if (matches.length === 0) {
+    notifyOfMissingMatches();
+    return;
+  }
+
+  totalHits = data.totalHits;
+  isEndReached = false;
+  notifyOfMatchesNum(data.totalHits);
+  renderMarkup(matches);
+  lightbox.refresh();
+  scrollToTop();
+  enableLoadMoreBtn();
+}
+
+function onLoadMoreFinished(data) {
+  renderMarkup(data.hits);
+  lightbox.refresh();
+  enableLoadMoreBtn();
+}
+
+function renderMarkup(list) {
+  const markup = photoCardsTpl(list);
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function makeInfScroll() {
+  const documentRect = document.documentElement.getBoundingClientRect();
+  if (documentRect.bottom < document.documentElement.clientHeight + 1400) {
+    onLoadMore();
+  }
+}
+
+function notifyOfMissingMatches() {
+  Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+}
+
+function notifyOfMatchesNum(totalHits) {
+  Notify.success(`Hooray! We found ${totalHits} images.`);
+}
+
+function notifyOfEndOfResults() {
+  Notify.info("We're sorry, but you've reached the end of search results.");
+}
+
+function clearGallery() {
+  refs.gallery.innerHTML = '';
+}
+
+function disableLoadMoreBtn() {
+  refs.loadMoreBtn.classList.add('hidden');
+}
+
+function enableLoadMoreBtn() {
+  refs.loadMoreBtn.classList.remove('hidden');
+}
+
+function scrollToTop() {
+  const { top: cardTop } = refs.gallery.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardTop - 100,
+    behavior: 'smooth',
+  });
+}
+
+function onError(error) {
+  console.dir(error);
+}
---
+++ src/js/query-api.js
@@ -0,0 +1,54 @@
+export default class QueryAPI {
+  #searchQuery;
+  #page;
+
+  constructor() {
+    this.#searchQuery = '';
+    this.#page = 1;
+    this.perPage = 100;
+  }
+
+  fetchImgs() {
+    const BASE_URL = 'https://pixabay.com/api/';
+    const API_KEY = '27133035-a58a151a38e3c6ba8d82de304';
+    const params = new URLSearchParams({
+      key: API_KEY,
+      q: this.searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: this.perPage,
+      page: this.page,
+    });
+
+    const url = `${BASE_URL}?${params}`;
+
+    this.#incrementPage();
+    return fetch(url).then(this.#onFetch);
+  }
+
+  getFetchedElsNum() {
+    return this.perPage * (this.page - 1);
+  }
+
+  setSearchQuery(newSearchQuery) {
+    this.searchQuery = newSearchQuery;
+    this.#resetPage();
+  }
+
+  #onFetch(response) {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    return response.json();
+  }
+
+  #resetPage() {
+    this.page = 1;
+  }
+
+  #incrementPage() {
+    this.page += 1;
+  }
+}
